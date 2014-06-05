aa6842cabe1b243ede152718c18f6f1bdc1a0780
Jamie McKenzie <jam@jamies-mbp.gateway.sonic.net>
2014-06-05 03:14:35 -0700

    Radio buttons filter for age.
    Selection does not clear upon new click.

--- app/assets/javascripts/pets.js
+++ app/assets/javascripts/pets.js
@@ -4,7 +4,43 @@
 $(document).ready(function() {
   var petsList = new List('pet-list', options);
 
-})
+
+  $('input:radio[value="Young"]').click(function(){
+    petsList.filter(function(item){
+       if (item.values().age === "Age: Young ") {
+           return true;
+       }
+       else {
+          return false;
+       }
+    });
+    return false;
+  });
+
+  $('input:radio[value="Senior"]').click(function(){
+    petsList.filter(function(item) {
+      if (item.values().age == "Age: Senior "){
+        return true;
+      }
+      else {
+        return false;
+      }
+    })
+    return false;
+  });
+
+  $('input:radio[value="Adult"]').click(function(){
+    petsList.filter(function(item) {
+      if (item.values().age == "Age: Adult "){
+        return true;
+      }
+      else {
+        return false;
+      }
+    })
+    return false;
+  });
+});
 
 var options = {
   valueNames: [ 'name', 'age', 'size', 'species' ],
